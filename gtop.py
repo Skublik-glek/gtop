3987a2b854054fe19ce088eea515756bd9baa43e
Skublik <82895701+Skublik-glek@users.noreply.github.com>
2022-07-25 14:41:16 +0300

    /

--- gtop.py
+++ gtop.py
@@ -6,6 +6,8 @@ all_info = {}
 sorted_info = {}
 print("Подождите, в последнее время API Mojang долго переводит uuid в ники...\n")
 data = requests.get("https://api.hypixel.net/guild?key=<<API КЛЮЧ>>&name=<<ИМЯ ГИЛЬДИИ>>").json()["guild"]["members"]
+
+
 for n, member in enumerate(data):
     try:
         name = requests.get(f"https://api.mojang.com/user/profiles/{member['uuid']}/names").json()[-1]["name"]
@@ -19,12 +21,15 @@ for n, member in enumerate(data):
     for date in member["expHistory"]:
         all_xp += member["expHistory"][date]
     all_info[name] = all_xp
+    
 
 sorted_keys = sorted(all_info, key=all_info.get)
 for w in reversed(sorted_keys):
     sorted_info[w] = all_info[w]
 
+    
 for n, key in enumerate(sorted_info.keys()):
     print(f"{n + 1}) {key}: {sorted_info[key]}")
 
+    
 water = input("\nНажмите Enter для завершения")
